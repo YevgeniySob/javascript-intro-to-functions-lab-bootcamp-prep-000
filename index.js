0223e9d98177a55444b57a1e41f3da3c4a81f894
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-01 19:59:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function logShout(str){
   return str.toLowerCase()
 }
 
-function
+function logShout(str){
+  console.log(str.toUpperCase())
+}
+function logWisper(str){
+  console.log(str.toLowerCase())
+}
+function sayHiToGrandma(str){
+  if(str.isLower)
+  return "I can't hear you!".toLowerCase()
+}
+function sayHiToGrand(str)
